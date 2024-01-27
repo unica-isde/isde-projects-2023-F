8b9808a2bd7ba0d006dee4132af692cad5c71dec
SimonePiras <47628357+SimonePiras@users.noreply.github.com>
2024-01-27 14:04:12 +0100

    Working download_scores button

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
+#from starlette.responses import StreamingResponse
+from fastapi import FastAPI
+import os
+from fastapi.responses import FileResponse
 import json
 from typing import Dict, List
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-import redis
-from rq import Connection, Queue
-from rq.job import Job
 from app.config import Configuration
 from app.forms.classification_form import ClassificationForm
 from app.ml.classification_utils import classify_image
 from app.utils import list_images
-from fastapi.responses import FileResponse
+#from fastapi.responses import FileResponse
 
 app = FastAPI()
 config = Configuration()
@@ -51,6 +52,8 @@ async def request_classification(request: Request):
     image_id = form.image_id
     model_id = form.model_id
     classification_scores = classify_image(model_id=model_id, img_id=image_id)
+    with open('classification_scores.json', 'w') as f:
+        json.dump(classification_scores, f)
     return templates.TemplateResponse(
         "classification_output.html",
         {
@@ -61,10 +64,10 @@ async def request_classification(request: Request):
     )
 
 
-"""
-@app.get("/")
-def main():
-    file_path = "large-video-file.mp4"
-    return FileResponse(path=file_path, filename=file_path, media_type='text/mp4')
 
-"""
+
+@app.get("/download_scores")
+async def download_scores():    
+    return FileResponse('classification_scores.json', media_type='application/json', filename='classification_scores.json')
+
+#@app.post("/download_plot")
