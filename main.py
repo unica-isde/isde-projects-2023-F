f01e2e50f01aab9f60d0b7aad840f9af622e403f
giopresenti98 <g.presenti@studenti.unica.it>
2024-03-05 17:44:26 +0100

    Completed the Assignment.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import json
 from typing import Dict, List
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, BackgroundTasks
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
@@ -11,6 +11,10 @@ from app.config import Configuration
 from app.forms.classification_form import ClassificationForm
 from app.ml.classification_utils import classify_image
 from app.utils import list_images
+from app.forms.transformation_form import TransformationForm
+from PIL import Image, ImageEnhance
+import os
+import time 
 
 
 app = FastAPI()
@@ -43,7 +47,6 @@ def create_classify(request: Request):
         {"request": request, "images": list_images(), "models": Configuration.models},
     )
 
-
 @app.post("/classifications")
 async def request_classification(request: Request):
     form = ClassificationForm(request)
@@ -59,3 +62,48 @@ async def request_classification(request: Request):
             "classification_scores": json.dumps(classification_scores),
         },
     )
+
+@app.get("/transformations")
+def create_transformation(request: Request):
+    return templates.TemplateResponse(
+        "transformation_select.html",
+        {"request": request, "images": list_images(), "models": Configuration.models},
+    )
+def delete_temp_img(temp_path):
+    time.sleep(1)
+    os.remove("app/static/imagenet_subset/" + temp_path)
+
+@app.post("/transformations")
+async def request_transformation(request: Request, background_tasks: BackgroundTasks):
+    form = TransformationForm(request)
+    await form.load_data()
+    image_id = form.image_id
+    model_id = form.model_id
+
+    with Image.open("app/static/imagenet_subset/" + image_id) as img:
+        img = ImageEnhance.Color(img).enhance(form.color)
+        img = ImageEnhance.Brightness(img).enhance(form.brightness)
+        img = ImageEnhance.Contrast(img).enhance(form.contrast)
+        img = ImageEnhance.Sharpness(img).enhance(form.sharpness)
+
+        temp_path = f"temp.{image_id}"
+        img.save("app/static/imagenet_subset/" + temp_path)
+        img.close()
+
+        classification_scores = classify_image(model_id = model_id, img_id = temp_path)
+
+        with open("app/static/transformedImage.json", "w") as json_file:
+            json.dump(classification_scores, json_file)
+            json_file.close()
+
+        response = templates.TemplateResponse(
+            "transformation_output.html",
+            {
+                "request": request,
+                "image_id": temp_path,
+                "classification_scores": json.dumps(classification_scores),
+            },
+        )
+
+        background_tasks.add_task(delete_temp_img, temp_path)
+        return response
