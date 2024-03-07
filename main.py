924beee287a1b4a722db3b913d5e697160b88b4f
SimonePiras <47628357+SimonePiras@users.noreply.github.com>
2024-03-07 11:09:04 +0100

    Code format edit

--- main.py
+++ main.py
@@ -1,37 +1,19 @@
-import numpy as np
 import matplotlib.pyplot as plt
-from fastapi import FastAPI
 import os
-from fastapi.responses import FileResponse
+from fastapi.responses import FileResponse, HTMLResponse
 import json
-
 from typing import Dict, List
-from fastapi import FastAPI, Request, BackgroundTasks
-from fastapi.responses import HTMLResponse
-import os
-from typing import Dict, List, Annotated
-from fastapi import FastAPI, Request, File, UploadFile
-from fastapi.responses import HTMLResponse, RedirectResponse
+from fastapi import FastAPI, Request, File, UploadFile, BackgroundTasks
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-import redis
 import shutil
-from rq import Connection, Queue
-from rq.job import Job
 from app.config import Configuration
 from app.forms.classification_form import ClassificationForm
 from app.ml.classification_utils import classify_image
 from app.utils import list_images, generate_histogram
-import matplotlib.pyplot as plt
-import numpy as np
-from app.utils import list_images
-
 from app.forms.transformation_form import TransformationForm
 from PIL import Image, ImageEnhance
-import os
-import time 
-
-
+import time
 from starlette.datastructures import URL
 
 app = FastAPI()
@@ -48,9 +30,9 @@ def info() -> Dict[str, List[str]]:
     list_of_images = list_images()
     list_of_models = Configuration.models
     data = {
-        "models": list_of_models, 
+        "models": list_of_models,
         "images": list_of_images
-        }
+    }
     return data
 
 
@@ -58,32 +40,34 @@ def info() -> Dict[str, List[str]]:
 def home(request: Request):
     """The home page of the service."""
     return templates.TemplateResponse(
-        "home.html", 
+        "home.html",
         {
             "request": request
-            }
-        )    
+        }
+    )
+
 
 @app.get("/classifications")
 def create_classify(request: Request):
     return templates.TemplateResponse(
         "classification_select.html",
         {
-            "request": request, 
-            "images": list_images(), 
-            "models": Configuration.models, 
+            "request": request,
+            "images": list_images(),
+            "models": Configuration.models,
             "userImage": 0
-            },
+        },
     )
+
 
 @app.post("/classifications")
 async def request_classification(
-        request: Request
-    ):
+    request: Request
+):
     form = ClassificationForm(request)
     await form.load_data()
-    image_id = form.image_id     
-    model_id = form.model_id       
+    image_id = form.image_id
+    model_id = form.model_id
     classification_scores = classify_image(model_id=model_id, img_id=image_id)
     with open('classification_scores.json', 'w') as f:
         json.dump(classification_scores, f)
@@ -92,22 +76,24 @@ async def request_classification(
         {
             "request": request,
             "image_id": image_id,
-            "classification_scores": json.dumps(classification_scores),            
-            "backButton" : "/classifications"
-        },        
+            "classification_scores": json.dumps(classification_scores),
+            "backButton": "/classifications"
+        },
     )
-    
+
+
 @app.get("/users_image")
 def create_classify(request: Request):
     return templates.TemplateResponse(
         "classification_select.html",
         {
-            "request": request, 
-            "images": list_images(), 
+            "request": request,
+            "images": list_images(),
             "models": Configuration.models,
             "userImage": 1
         },
     )
+
 
 @app.post("/users_image")
 async def users_image(request: Request):
@@ -115,16 +101,17 @@ async def users_image(request: Request):
     await form.load_data()
     image_id = "n00000000_usersImage.JPEG"
     model_id = form.model_id
-    classification_scores = classify_image(model_id=model_id, img_id=image_id)   
+    classification_scores = classify_image(model_id=model_id, img_id=image_id)
     return templates.TemplateResponse(
         "classification_output.html",
         {
             "request": request,
             "image_id": image_id,
             "classification_scores": json.dumps(classification_scores),
-            "backButton" : "/users_image"
+            "backButton": "/users_image"
         },
     )
+
 
 @app.get("/histograms")
 def create_histograms(request: Request):
@@ -132,6 +119,7 @@ def create_histograms(request: Request):
         "histogram_select.html",
         {"request": request, "images": list_images()},
     )
+
 
 @app.post("/histograms")
 async def request_histogram(request: Request):
@@ -147,7 +135,8 @@ async def request_histogram(request: Request):
         {
             "request": request,
             "image_id": image_id,
-            "histogram_base64": histogram_base64,  # Pass the histogram base64 to the context
+            # Pass the histogram base64 to the context
+            "histogram_base64": histogram_base64,
         },
     )
 
@@ -186,7 +175,8 @@ async def request_transformation(request: Request, background_tasks: BackgroundT
         img.save("app/static/imagenet_subset/" + temp_path)
         img.close()
 
-        classification_scores = classify_image(model_id = model_id, img_id = temp_path)
+        classification_scores = classify_image(
+            model_id=model_id, img_id=temp_path)
 
         # Save the classification scores to a JSON file
         with open("app/static/transformedImage.json", "w") as json_file:
@@ -207,12 +197,13 @@ async def request_transformation(request: Request, background_tasks: BackgroundT
         background_tasks.add_task(delete_temp_img, temp_path)
         return response
 
+
 @app.post("/upload/")
 async def create_upload_file(
     request: Request,
     image_id: UploadFile = File(...)
-):   
-    if(
+):
+    if (
         image_id.filename.endswith(".jpg") |
         image_id.filename.endswith(".JPEG") |
         image_id.filename.endswith(".jpeg") |
@@ -221,11 +212,12 @@ async def create_upload_file(
     ):
         with open("./app/static/imagenet_subset/n00000000_usersImage.JPEG", "wb") as buffer:
             shutil.copyfileobj(image_id.file, buffer)
-            form = ClassificationForm(request)      
+            form = ClassificationForm(request)
         await form.load_data()
         image_id = "n00000000_usersImage.JPEG"
         model_id = form.model_id
-        classification_scores = classify_image(model_id=model_id, img_id=image_id)
+        classification_scores = classify_image(
+            model_id=model_id, img_id=image_id)
         request._url = URL("/classifications")
         return templates.TemplateResponse(
             "classification_output.html",
@@ -233,30 +225,32 @@ async def create_upload_file(
                 "request": request,
                 "image_id": image_id,
                 "classification_scores": json.dumps(classification_scores),
-                "backButton" : "/delete"
+                "backButton": "/delete"
             },
         )
     else:
         return templates.TemplateResponse(
-           "classification_select.html",
+            "classification_select.html",
             {
-                "request": request, 
-                "images": list_images(), 
+                "request": request,
+                "images": list_images(),
                 "models": Configuration.models,
                 "userImage": 1
             },
         )
 
+
 @app.get("/delete")
-async def deleteFile(request : Request):
-    if(os.path.isfile("./app/static/imagenet_subset/n00000000_usersImage.JPEG")):
+async def deleteFile(request: Request):
+    if (os.path.isfile("./app/static/imagenet_subset/n00000000_usersImage.JPEG")):
         os.remove("./app/static/imagenet_subset/n00000000_usersImage.JPEG")
     return templates.TemplateResponse(
-            "deleteFile.html",
-            {
-                "request": request,
-            }
-        )
+        "deleteFile.html",
+        {
+            "request": request,
+        }
+    )
+
 
 @app.get("/download_scores")
 async def download_scores():
@@ -297,4 +291,3 @@ async def download_plot():
     plt.close()
 
     return FileResponse('classification_plot.png', media_type='application/png', filename='classification_plot.png')
-
